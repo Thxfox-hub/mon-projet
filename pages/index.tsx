d9034aa5f307081d281160f1098c51ee2eda45ed
avictor.garcia777 <avictor.garcia777@gmail.com>
2024-10-05 07:53:47 +0000

    Initialisation du projet

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur mon application Next.js légère</h1>
+      <p>Ceci est une application minimaliste pour démarrer avec Next.js.</p>
+    </div>
+  );
+}
